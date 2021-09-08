d5550fc6e82bc887c424ca746051f9832af5a966
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2021-09-08 14:28:41 +0200

    Initial commit

---
+++ src/sample/ClientTemp.java
@@ -0,0 +1,51 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientTemp extends Application {
+    DataOutputStream toServer = null;
+    DataInputStream fromServer = null;
+
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane mainPane = new BorderPane();
+        TextArea ta = new TextArea();
+        mainPane.setCenter(new ScrollPane(ta));
+
+        Scene scene = new Scene(mainPane, 450, 200);
+        primaryStage.setTitle("Client");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        try {
+            Socket socket = new Socket("10.200.130.36", 22);
+
+            fromServer = new DataInputStream(socket.getInputStream());
+            toServer = new DataOutputStream(socket.getOutputStream());
+
+        } catch (
+                IOException ex){
+            System.out.println("ERROR!!");
+            ta.appendText(ex.toString() + '\n');
+        }
+
+        try {
+            String result = fromServer.readUTF();
+            System.out.println("Result is: " + result);
+            ta.appendText(result + "\n");
+        } catch (IOException ex) {
+            System.out.println("Error2!!");
+            ex.printStackTrace();
+        }
+    }
+}
